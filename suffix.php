25ecca57c5aacf7c7ee31c9bdfe87bad767a994a
Ivanov I <ilia@ivanov.press>
2021-03-17 23:07:58 +0300

    FIX: 11-14 баг

--- suffix.php
+++ suffix.php
@@ -3,16 +3,20 @@
 
 // Создайте программу, которая выведет в нужной форме текстовое сообщение, например такие “на учебе 100 студентов”, или “на учебе 2 студента” и т.д.
 
-$studentsCount = rand(1, 1000000);
+$studentsCount = rand(10, 15);
 
 function howMuchStudentsNow ($count)
 {
-    $lastInt = $count % 10;
-    if ($lastInt === 1) {
-        $suffix = '';
-    } elseif ($lastInt > 1 && $lastInt < 5) {
-        $suffix = 'а';
-    } else $suffix = 'ов';
+    $lastInt = $count % 100;
+    if ($lastInt === 11 || $lastInt === 12 || $lastInt === 13 || $lastInt === 14) {
+        $suffix = 'ов';
+    } else {
+        if ($lastInt % 10 === 1) {
+            $suffix = '';
+        } elseif ($lastInt % 10 > 1 && $lastInt % 10 < 5) {
+            $suffix = 'а';
+        } else $suffix = 'ов';
+    }
 
     echo "на учебе $count студент$suffix";
 }
